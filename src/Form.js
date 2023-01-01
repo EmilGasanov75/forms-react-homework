316018b3e2139e767fcd34f03515e37047dc42c4
cTPaXMocKaLiB <beanosbeanosovich@gmail.com>
2023-01-01 18:57:01 +0100

    hw1

---
+++ src/Form.js
@@ -0,0 +1,34 @@
+import { useState } from "react";
+
+function Form() {
+    const [err, setErr] = useState(false)
+    const [formData, setFromData] = useState({})
+    const onSubmit = function(e) {
+        e.preventDefault()
+        setErr(true)
+        console.log()
+    }
+    const onChange = function(e){
+        let data = e.currentTarget.value
+        let name = e.currentTarget.name
+        setFromData(prev => {
+            return({
+                ...prev, [name]: data
+            })
+        }) 
+    }
+    return(
+        <form onSubmit={onSubmit}>
+            <input onChange={onChange} type="email" name="email"/>
+            {!formData.email && err ? "Введіть будь ласка свій email":""}
+            <input onChange={onChange} type="text" name="nickname"/>
+            {!formData.nickname && err ? "Введіть будь ласка свій nickname":""}
+            <input onChange={onChange} type="text" name="adress"/>
+            {!formData.adress && err ? "Введіть будь ласка свій adress":""}
+            <button>Submit</button>
+            <p>{err}</p>
+        </form>
+    )
+}
+
+export default Form
